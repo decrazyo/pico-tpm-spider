d92f62ff7cdfbf34ba95683fc14ead13ef05d80e
dale <decrazyo@gmail.com>
2026-02-11 11:48:16 -0600

    improve performance, parsing, and docs

--- README.md
+++ README.md
@@ -1,37 +1,77 @@
 
 # Pico TPM SPIder
 
-Pico TPM SPIder is a Volume Master Key (VMK) recovery tool for SPI-based TPM 2.0 chips, built on Raspberry Pi Pico hardware.
+Pico TPM SPIder is a Volume Master Key (VMK) recovery tool for SPI-based TPM 2.0 chips, built on [Raspberry Pi Pico](https://www.digikey.com/en/products/detail/raspberry-pi/SC0915/13624793) hardware.
 It passively monitors SPI communication with a discrete TPM 2.0 chip and parses transactions in real time to extract the VMK.
-This project specifically targets the STMicroelectronics [ST33TPHF2XSPI](https://www.st.com/en/product/st33tphf2xspi) used in Dell Latitude 5320 laptops,
+This project specifically targets the STMicroelectronics [ST33TPHF2XSPI](https://www.st.com/en/product/st33tphf2xspi) used in several Dell laptops,
 though other devices and TPM 2.0 chips may be compatible.
+
+Pico TPM SPIder is designed to capture TPM SPI traffic clocked at 25MHz to 50MHz with or without the use of a chip select signal.
+Lower frequencies can be supported by modifying [sniffer.pio](https://github.com/decrazyo/pico-tpm-spider/blob/main/sniffer.pio).
+Higher frequencies may be supported through overclocking.
+
+<!-- TODO: add link to cyphercon talk -->
+
+## Compatibility
+
+The following devices have been tested.
+
+| device              | working | bus voltage | chip select  | notes |
+|---------------------|---------|-------------|--------------|-------|
+| Dell Latitude 5320  | yes     | 3.3V        | inaccessible | SPI flash memory, unpopulated SPI connector |
+| Dell Latitude 5431  | yes     | 3.3V        | inaccessible | SPI flash memory |
+| Dell Precision 3490 | yes     | 1.8V        | inaccessible | SPI flash memory |
+
+> [!CAUTION]
+> Check the voltage of your target device's SPI bus!
+> The Raspberry Pi Pico is only designed to work with 3.3V logic.
+> A converter should be used to interface with other bus voltages.
+> The [TXS0108E](https://www.amazon.com/TXS0108E-Bi-Directional-Converter-Channel-Conversion/dp/B0F285G5JT) has been proven to work for this.
 
 ## Hardware
 
-A custom PCB is provided in the `hardware` directory that simplifies connecting to the SPI bus on Dell Latitude 5320 laptops via an unpopulated connector.
-<!-- TODO: insert image pcb design -->
-<!-- TODO: insert image of laptop motherboard -->
-The PCB provides two methods for interfacing with the motherboard.
+The following methods of connecting to the SPI bus have been tested.
 
-1) **Pogo Pins**  
+### WSON8 Probe
+
+All of the tested devices have a [SPI flash memory chip](https://www.winbond.com/resource-files/W25R256JV%20RevD%2006162020.pdf) on the same bus as the TPM chip.
+A [WSON8 probe](https://www.amazon.com/Amagogo-Download-Efficient-Industrial-Electronic/dp/B0DRZGRN81) can be used to connect to the SPI bus via the flash chip.
+This is by far the easiest way to connect to the SPI bus.
+
+![WSON8 SPI flash memory chip pinout](https://github.com/decrazyo/pico-tpm-spider/blob/main/img/wson8.png)
+
+### Custom PCB
+
+A custom PCB, inspired by [stacksmashing's design](https://github.com/stacksmashing/pico-tpmsniffer), is provided in the `hardware` directory and simplifies connecting to the SPI bus on Dell Latitude 5320 laptops via an unpopulated connector.
+The PCB provides two methods for interfacing with the connector.
+
+![Unpopulated SPI connector pinout](https://github.com/decrazyo/pico-tpm-spider/blob/main/img/connector.png)
+
+1) **Pogo Pins** (hard to solder, easy to use)  
 The board includes footprints for pogo pins that align with the Latitude 5320's unpopulated connector.
-<!-- TODO: insert image of pogo pins being used -->
 
-2) **Flat Flex Ribbon**  
-The board accommodates a flat flex ribbon connector. A flat flex ribbon can be connected to the PCB and then pressed against the Latitude 5320's unpopulated connector.
-<!-- TODO: insert image of ribbon being used -->
+2) **Flat Flex Ribbon** (easy to solder, hard to use)  
+The board accommodates a flat flex ribbon connector.
+A flat flex ribbon can be connected to the PCB and then pressed against the Latitude 5320's unpopulated connector.
 
 Since the unpopulated connector does not include a chip select (CS) signal, the PCB grounds the Pi Pico's CS input (GPIO 3) by default.
 Cutting jumper `JP1` will disconnect the CS input from GND.  
 
-
-### Parts
 | description                | part number |
 |----------------------------|-------------|
-| Raspberry Pi Pico          | [SC0915](https://www.digikey.com/en/products/detail/raspberry-pi/SC0915/13624793) |
 | Pogo Pins                  | [P50-B1-16mm](https://www.amazon.com/KooingTech-Pressure-Testing-Fingers-Printed/dp/B0DMGG23QF) |
 | Flat Flex Ribbon           | [0982660209](https://www.digikey.com/en/products/detail/molex/0982660209/2750458) |
 | Flat Flex Ribbon Connector | [CF31201D0R0-05-NH](https://www.digikey.com/en/products/detail/cvilux-usa/CF31201D0R0-05-NH/15792943) |
+
+### Needle Probes / Soldering
+
+[Needle probes](https://sensepeek.com/4x-sq10-probes-with-test-wires) can be used to connect to any exposed part of the SPI bus.
+Such a setup can be precarious so soldering may be a better option if you have some [30 gauge wire](https://www.amazon.com/Jonard-R-30B-0050-Replacement-Kynar-Dispenser/dp/B006C4ABR0) and a steady hand.
+
+> [!NOTE]
+> Interference can cause various issues including
+> failure to boot, entering Dell's "manufacturing mode", BIOS/UEFI corruption, and failure to recover the VMK.
+> Keep your SPI wires short, away from sources of interference, and preferably shielded to avoid those issues.
 
 ## Build
 
@@ -67,8 +107,9 @@ Connect the Pi Pico's pins to your TPM chip according to the following table.
 | GPIO 3   | CS or GND  |
 
 The use of a chip select (CS) signal is optional but recommenced since it significantly reduces the amount of data that the Pi Pico needs to parse.
-**If you're not using the CS signal then GPIO 3 must be grounded!**
-GPIO 3 is grounded by default on the provided PCB.  
+> [!IMPORTANT]
+> If you're not providing the Pi Pico with the TPM's CS signal then GPIO 3 must be grounded!  
+> GPIO 3 is grounded by default on the provided PCB.  
 
 Connect to the Pi Pico with your serial terminal of choice and boot the target system.  
 
@@ -79,7 +120,7 @@ picocom -q /dev/ttyACM0 -b 115200
 You should see output similar to this.
 
 > Capturing SPI traffic into 12 16384-byte buffers.  
-> Press any key to finalize the capture.  
+> Press any key to finalize the capture and reboot.  
 > VMK: DEADC0DEDEADC0DEDEADC0DEDEADC0DEDEADC0DEDEADC0DEDEADC0DEDEADC0DE  
 
 If the VMK does not automatically appear by the time the system has booted then press a key to finalize the capture.
@@ -97,7 +138,9 @@ ls /mnt/drive_c/
 ```
 
 ## Related Projects
+* https://github.com/decrazyo/spi-analyzer
 * https://github.com/ReversecLabs/bitlocker-spi-toolkit
+* https://github.com/NoobieDog/TPM-Sniffing
 * https://github.com/stacksmashing/pico-tpmsniffer
 * https://github.com/zaphoxx/pico-tpmsniffer-spi
 * https://github.com/aplhk/pico-spisniffer
--- main.c
+++ main.c
@@ -3,18 +3,11 @@
 #include "hardware/clocks.h"
 #include "hardware/dma.h"
 #include "hardware/pio.h"
+#include "hardware/watchdog.h"
 #include "pico/multicore.h"
 #include "pico/stdlib.h"
 #include <tusb.h>
 #include <stdio.h>
-
-// uncomment this to use the built-in LED to indicate a buffer overrun.
-// the LED will turn on if parsing isn't keeping up with the data streaming in.
-// the LED will turn off again if parsing manages to catch up and parse all currently buffered data.
-// if a buffer overrun occurs then the LED will remain on until the Pi Pico is reset.
-// buffer overruns are unlikely and a volume master key may still be recovered even if a buffer overrun occurs.
-// leaving this functionality disabled provides a slight performance increase.
-// #define DEBUG_LED
 
 // volume master key length in bytes.
 #define VMK_LENGTH_BYTES 32
@@ -37,24 +30,24 @@
 
 // number of buffers for a given signal.
 // each buffer is serviced by a dedicated DMA channel.
-// minimum: 2
-// maximum: 6
 #define BUFFERS_PER_SIGNAL 6
+_Static_assert(BUFFERS_PER_SIGNAL >= 2 && BUFFERS_PER_SIGNAL <= 6, "BUFFERS_PER_SIGNAL must be in range [2, 6]");
 
 // length of each buffer as a power of 2.
-// minimum: 2^1
-// maximum: 2^15
 #define BUFFER_LENGTH_POWER 14
+_Static_assert(BUFFER_LENGTH_POWER >= 1 && BUFFER_LENGTH_POWER <= 15, "BUFFER_LENGTH_POWER must be in range [1, 15]");
 
 // length of each buffer in bytes.
 #define BUFFER_LENGTH_BYTES (1 << BUFFER_LENGTH_POWER)
 
-// number of bits the SPI sniffer must receive before auto-pushing them into the RX FIFO.
+// amount of data the SPI sniffer must receive before auto-pushing them into the RX FIFO.
 // the TPM data that we're interested in is transmitted as 8-bit bytes.
-#define AUTOPUSH_THRESHOLD 8
+#define AUTOPUSH_THRESHOLD_BYTES 1
+#define AUTOPUSH_THRESHOLD_BITS AUTOPUSH_THRESHOLD_BYTES * 8
 
 // base GPIO pin for the PIO state machines.
-// note that CLK and CS are defined as fixed GPIO pins in "snuffer.pio".
+// note that CLK and CS are defined as fixed GPIO pins in "snuffer.pio"
+// so you'll need to update that too if you want to change the base pin.
 #define BASE_PIN 0
 
 // offsets from BASE_PIN.
@@ -69,105 +62,108 @@ enum spi_pin {
 // we'll cycle through these DMA channels to buffer SPI data.
 static int mosi_channels[BUFFERS_PER_SIGNAL];
 static int miso_channels[BUFFERS_PER_SIGNAL];
+
 // these buffers need to be naturally-aligned.
 // see "channel_config_set_ring" documentation for more details.
 static uint8_t mosi_buffers[BUFFERS_PER_SIGNAL][BUFFER_LENGTH_BYTES] __attribute__((aligned(BUFFER_LENGTH_BYTES)));
 static uint8_t miso_buffers[BUFFERS_PER_SIGNAL][BUFFER_LENGTH_BYTES] __attribute__((aligned(BUFFER_LENGTH_BYTES)));
 
 // we always start by writing to buffer 0.
-static volatile uint8_t dma_write_buffer = 0;
 // start reading at the end of the last buffer.
 // logic in "advance_to_next_capture_blocking" will initially swap to reading buffer 0
 // and wait for DMA to finish writing to it before reading from it.
-static uint8_t dma_read_buffer = BUFFERS_PER_SIGNAL - 1;
-static size_t dma_read_index = BUFFER_LENGTH_BYTES - 1;
-
-#ifdef DEBUG_LED
-// tracks if our buffer reading code is keeping up with the DMA writes.
-static volatile int32_t dma_write_count = 0;
-static int32_t dma_read_count = 0;
-#endif
+static uint8_t read_buffer = BUFFERS_PER_SIGNAL - 1;
+static size_t read_index = BUFFER_LENGTH_BYTES - 1;
+static size_t read_length = BUFFER_LENGTH_BYTES;
 
 // flag indicating if the user would like to stop capturing and process any remaining data.
 static bool end_capture = false;
 
 // advance to the next pair of synchronously captured bytes from MOSI and MISO.
 // calls to this function will block until data becomes available.
-// if capturing has been stopped by the user then this function may block forever.
+// if capturing has been stopped by the user then this function may force a reboot.
 // see also:
-//      dma_irq0_handler
 //      parse_tpm_transactions
 static inline void advance_to_next_capture_blocking() {
-    dma_read_index++;
+    read_index++;
 
     // check if we are about to read past the end of the buffer.
-    if(dma_read_index >= BUFFER_LENGTH_BYTES) {
+    if(read_index >= read_length) {
+
+        // check if the user wants to stop capturing.
         if(end_capture) {
             printf("Done\n");
-            while(true) {}
+            // the user may want to attempt another capture after this.
+            // therefore, we should return to an initial known good state.
+            // rebooting is the most reliable way to do that.
+            printf("Rebooting\n");
+            watchdog_reboot(0, 0, 200);
+            while (true) {}
         }
 
         // switch to reading at the start of the next buffer.
-        dma_read_index = 0;
-        dma_read_buffer++;
-        dma_read_buffer %= BUFFERS_PER_SIGNAL;
+        read_index = 0;
+        read_buffer++;
+        read_buffer %= BUFFERS_PER_SIGNAL;
 
         // wait for DMA to finish writing to the buffer that we want to read.
-        // it's possible for all DMA channels to briefly be inactive so we can't just use
-        // "dma_channel_is_busy" and "dma_channel_wait_for_finish_blocking" for this.
-        while(dma_read_buffer == dma_write_buffer) {
+        while(dma_channel_is_busy(mosi_channels[read_buffer])) {
             // while we're waiting, check if the user wants us to finish capturing.
             if(stdio_getchar_timeout_us(0) != PICO_ERROR_TIMEOUT) {
                 // user pressed a key.
                 printf("Finalizing\n");
+
+                // abort all dma channels.
+                // don't bother waiting for them to actually abort.
+                dma_hw->abort = 0xFFF;
+
+                // determine how much of the buffer was written.
+                read_length = BUFFER_LENGTH_BYTES / AUTOPUSH_THRESHOLD_BYTES - dma_hw->ch[read_buffer].transfer_count;
+
+                // set a flag to indicate we're done capturing.
                 end_capture = true;
-                // parse whatever is in the buffer even if DMA hasn't finished writing to it.
                 break;
             }
         }
-
-#ifdef DEBUG_LED
-        // if we aren't keeping up with the data streaming in then the built-in LED will turn on.
-        // if we're able to catch up again then the LED should turn off.
-        dma_read_count++;
-        gpio_put(PICO_DEFAULT_LED_PIN, dma_read_count != dma_write_count);
-#endif
     }
-}
-
-// this interrupt handler is called when a DMA channel finishes its transfer.
-// used to indicate which buffer is currently being written to.
-// see also:
-//      advance_to_next_capture_blocking
-void dma_irq0_handler() {
-    dma_channel_acknowledge_irq0(mosi_channels[dma_write_buffer]);
-#ifdef DEBUG_LED
-    dma_write_count++;
-#endif
-    dma_write_buffer++;
-    dma_write_buffer %= BUFFERS_PER_SIGNAL;
 }
 
 // get the value of MOSI from the current point in the captured data.
 // see also:
 //      advance_to_next_capture_blocking
 static inline uint8_t get_current_mosi_byte() {
-    return mosi_buffers[dma_read_buffer][dma_read_index];
+    return mosi_buffers[read_buffer][read_index];
 }
 
 // get the value of MOSI from the current point in the captured data.
 // see also:
 //      advance_to_next_capture_blocking
 static inline uint8_t get_current_miso_byte() {
-    return miso_buffers[dma_read_buffer][dma_read_index];
+    return miso_buffers[read_buffer][read_index];
 }
 
-// parse captured SPI data for a subset of TPM transactions that contain the VMK.
+// parse captured SPI data for TPM transactions that are known to contain VMK data.
 // we only care about the TPM host reading from address 0xD40024 (TPM_DATA_FIFO_0).
-// the transactions we're parsing should have the following structure.
-// MOSI:  0x80, 0xD4, 0x00, 0x24, 0x00, 0x00    0x80, 0xD4, 0x00, 0x24, 0x00, 0x00
-// MISO:  0x80, 0x00, 0x00, 0x00, 0x01, 0x??    0x80, 0x00, 0x00, 0x00, 0x01, 0x??
-// where "0x??" is a data byte to be passed to the Pi Pico's second core for further parsing.
+// according to the ST33TPHF2XSPI datasheet, those transactions have the following structure.
+//
+//     read    data length - 1
+//         \  /  ,- address --,    ,------- garbage --------,
+//          ||  /              \  /                          \
+// MOSI:  0x82, 0xD4, 0x00, 0x24, 0x??, 0x??, 0x??, 0x??, 0x??
+// MISO:  0x??, 0x??, 0x??, 0x00, 0x00, 0x01, 0x??, 0x??, 0x??
+//        \              /  \        /  \  /  \              /
+//         '- garbage --'   waits >= 1  ack    '--- data ---'
+//
+// this parser is designed to handle that structure
+// but in testing, all relevant transactions looked exactly like this.
+//
+//     read    data length 0 -> 1 byte.
+//         \  /  ,- address --,             
+//          ||  /              \            
+// MOSI:  0x80, 0xD4, 0x00, 0x24, 0x00, 0x00
+// MISO:  0x80, 0x00, 0x00, 0x00, 0x01, 0x??
+//                          \  /  \  /  \  /
+//                          wait  ack   data
 //
 // see also:
 //      advance_to_next_capture_blocking
@@ -220,14 +216,22 @@ static inline void parse_tpm_transactions() {
         }
 
         // the TPM host is trying to read address 0xD40024 (TPM_DATA_FIFO_0).
-        // if the TPM isn't ready yet then it may respond with 1 or more "wait" bytes.
-        // in testing, "wait" bytes were never seen when the TPM transmitted the VMK and VMK header
-        // but the ST33TPHF2XSPI datasheet says it's allowed so we'll check for it.
+
+        // at this point, the TPM should have already sent 1 wait byte.
+        if(get_current_miso_byte() != TPM_WAIT) {
+            // found unexpected byte. reset the parser.
+            continue;
+        }
+
+        // if the TPM isn't ready yet then it may respond with more wait bytes.
+        // the ST33TPHF2XSPI datasheet suggests that this is only the case for write requests
+        // and additional wait bytes were not seen during testing.
+        // we'll check for them anyway.
         do {
             advance_to_next_capture_blocking();
         } while(get_current_miso_byte() == TPM_WAIT);
 
-        // the TPM stopped sending "wait" bytes.
+        // the TPM stopped sending wait bytes.
         // check MISO to see if the TPM acknowledged the read request.
         if(get_current_miso_byte() != TPM_ACK) {
             // found unexpected byte. reset the parser.
@@ -236,16 +240,10 @@ static inline void parse_tpm_transactions() {
 
         // the TPM acknowledged the request.
         // pass the following read_count+1 bytes to the other core for further processing.
-
-        // always read at least 1 data byte.
-        advance_to_next_capture_blocking();
-        multicore_fifo_push_blocking(get_current_miso_byte());
-
-        // read as many additional data bytes as the TPM host requested.
-        for(; read_count; read_count--) {
+        do {
             advance_to_next_capture_blocking();
             multicore_fifo_push_blocking(get_current_miso_byte());
-        }
+        } while(read_count--);
 
         // get ready to parse the next transaction.
         advance_to_next_capture_blocking();
@@ -325,21 +323,34 @@ void core1_dump_vmk() {
 }
 
 // initialize a PIO state machine.
-void sniffer_program_init(PIO pio, int sm, int offset, uint in_pin, uint jmp_pin) {
+void sniffer_program_init(PIO pio, int sm, int offset, uint in_pin) {
+    // configure pins as inputs.
     pio_sm_set_consecutive_pindirs(pio, sm, BASE_PIN, SPI_PIN_COUNT, false);
 
     pio_sm_config sm_config = sniffer_program_get_default_config(offset);
+
+    // read data from MOSI or MISO.
     sm_config_set_in_pins(&sm_config, in_pin);
-    sm_config_set_jmp_pin(&sm_config, jmp_pin);
-    sm_config_set_in_shift(&sm_config, false, true, AUTOPUSH_THRESHOLD);
+
+    // use the CLK signal for control flow.
+    sm_config_set_jmp_pin(&sm_config, BASE_PIN + SPI_PIN_CLK);
+
+    // configure the ISR to receive MSB first and auto-push every 8 bits to the RX FIFO.
+    sm_config_set_in_shift(&sm_config, false, true, AUTOPUSH_THRESHOLD_BITS);
+
+    // join the RX and TX FIFO queues into a single large RX FIFO queue.
+    // this isn't strictly necessary since DMA will be rapidly draining the queue.
+    // it don't hurt though.
     sm_config_set_fifo_join(&sm_config, PIO_FIFO_JOIN_RX);
+
+    // run the state machine at full CPU clock speed.
     sm_config_set_clkdiv(&sm_config, 1.0);
 
     pio_sm_init(pio, sm, offset, &sm_config);
 }
 
 // initialize DMA channels.
-void dma_channels_init(PIO pio, int sm, int dma_channels[BUFFERS_PER_SIGNAL], uint8_t dma_buffers[BUFFERS_PER_SIGNAL][BUFFER_LENGTH_BYTES], bool irq_enabled) {
+void dma_channels_init(PIO pio, int sm, int dma_channels[BUFFERS_PER_SIGNAL], uint8_t dma_buffers[BUFFERS_PER_SIGNAL][BUFFER_LENGTH_BYTES]) {
     // claim 1 DMA channel per buffer.
     for(size_t channel_index = 0; channel_index < BUFFERS_PER_SIGNAL; channel_index++) {
         dma_channels[channel_index] = dma_claim_unused_channel(true);
@@ -350,15 +361,35 @@ void dma_channels_init(PIO pio, int sm, int dma_channels[BUFFERS_PER_SIGNAL], ui
         size_t chain_index = (channel_index + 1) % BUFFERS_PER_SIGNAL;
 
         dma_channel_config channel_config = dma_channel_get_default_config(dma_channels[channel_index]);
+
+        // reading from the RX FIFO uses the same address for every read.
+        // don't automatically increment the read address.
         channel_config_set_read_increment(&channel_config, false);
+
+        // writing fills a memory buffer.
+        // automatically increment the write address.
         channel_config_set_write_increment(&channel_config, true);
+
+        // transfer 1 byte at a time.
         channel_config_set_transfer_data_size(&channel_config, DMA_SIZE_8);
+
+        // the DMA channel will read from the state machine's RX FIFO at the state machine's request.
         channel_config_set_dreq(&channel_config, pio_get_dreq(pio, sm, false));
+
+        // treat the memory buffer as a ring.
+        // i.e. reset the write address to the start of the buffer upon completion.
+        // the memory buffer needs to be naturally-aligned for this to work.
         channel_config_set_ring(&channel_config, true, BUFFER_LENGTH_POWER);
-        channel_config_set_enable(&channel_config, true);
+
+        // trigger the next DMA channel to start upon completion of this one.
+        // DMA channels will automatically trigger each other in a loop.
+        // this effectively lets the DMA controller continuously fill a giant buffer with zero CPU overhead.
         channel_config_set_chain_to(&channel_config, dma_channels[chain_index]);
 
-        dma_channel_set_irq0_enabled(dma_channels[channel_index], irq_enabled);
+        // allow the DMA channel to be started.
+        channel_config_set_enable(&channel_config, true);
+
+        // apply the DMA channel configuration but don't start it yet.
         dma_channel_configure(
             dma_channels[channel_index],
             &channel_config,
@@ -393,23 +424,18 @@ int main() {
     int offset = pio_add_program(pio, &sniffer_program);
 
     // initialize 2 PIO state machines, one for capturing MOSI and the other for capturing MISO.
-    sniffer_program_init(pio, mosi_sm, offset, BASE_PIN + SPI_PIN_MOSI, BASE_PIN + SPI_PIN_CLK);
-    sniffer_program_init(pio, miso_sm, offset, BASE_PIN + SPI_PIN_MISO, BASE_PIN + SPI_PIN_CLK);
+    sniffer_program_init(pio, mosi_sm, offset, BASE_PIN + SPI_PIN_MOSI);
+    sniffer_program_init(pio, miso_sm, offset, BASE_PIN + SPI_PIN_MISO);
 
     // setup DMA channels to buffer data from the state machines.
-    // only the DMA channels servicing the MOSI state machine will trigger interrupts.
-    dma_channels_init(pio, mosi_sm, mosi_channels, mosi_buffers, true);
-    dma_channels_init(pio, miso_sm, miso_channels, miso_buffers, false);
-
-    // assign an interrupt handler to manage buffer access.
-    irq_set_exclusive_handler(DMA_IRQ_0, dma_irq0_handler);
-    irq_set_enabled(DMA_IRQ_0, true);
+    dma_channels_init(pio, mosi_sm, mosi_channels, mosi_buffers);
+    dma_channels_init(pio, miso_sm, miso_channels, miso_buffers);
 
     // synchronously start the 0th DMA channels for each signal.
     // DMA chaining will take care of starting other channels as needed.
     dma_start_channel_mask((1 << mosi_channels[0]) | (1 << miso_channels[0]));
 
-    // get the other core ready to find the VMK.
+    // get the other CPU core ready to find the VMK.
     multicore_launch_core1(core1_dump_vmk);
 
     // turn off the led to indicate that initialization is done.
@@ -420,16 +446,12 @@ int main() {
         sleep_ms(100);
     }
 
-#ifdef DEBUG_LED
-    printf("Debug LED enabled.\n");
-#endif
-
     printf("Capturing SPI traffic into %u %u-byte buffers.\n",
         BUFFERS_PER_SIGNAL * SIGNAL_COUNT,
         BUFFER_LENGTH_BYTES
     );
 
-    printf("Press any key to finalize the capture.\n");
+    printf("Press any key to finalize the capture and reboot.\n");
 
     // start synchronously capturing SPI data.
     pio_enable_sm_mask_in_sync(pio, (1 << mosi_sm) | (1 << miso_sm));
